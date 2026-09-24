ce41edd59fc9595353bcae354cdfc54b4af4f80f
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2026-09-25 02:56:55 +1000

    Set SVT leaves to NULL for empty columns when loading MatrixMarket data. (#133)
    Mostly backports of the updated C++ code and associated tests from crio.

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,1 +1,1 @@
-safeBPPARAM <- if (.Platform$OS.type=="windows") BiocParallel::SerialParam() else BiocParallel::MulticoreParam(3)
+safeBPPARAM <- if (.Platform$OS.type=="windows") BiocParallel::SerialParam() else BiocParallel::MulticoreParam(2)
